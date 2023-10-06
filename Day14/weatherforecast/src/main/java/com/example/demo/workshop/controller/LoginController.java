df00c571e33f48f34d63b7191264bbc0b3beabed
kabarthini <110531712+kabarthini@users.noreply.github.com>
2023-10-06 15:36:47 +0530

    Add files via upload

---
+++ Day14/weatherforecast/src/main/java/com/example/demo/workshop/controller/LoginController.java
@@ -0,0 +1,30 @@
+//package com.example.demo.workshop.controller;
+//
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//
+//import com.example.demo.workshop.entity.Login;
+//import com.example.demo.workshop.service.LoginService;
+//
+//
+//public class LoginController {
+	//@Autowired
+//	private LoginService ls;
+//	@GetMapping("/getuser")
+//  public List<Login>getLoginDetails1()
+//  {
+//  	return ls.getUser();
+//  }
+//	
+//  @PostMapping("/postuser")
+//  public String saveDetails(@RequestBody Login le)
+//  {
+//  	us.saveUser(le);
+//  	return "Successfully saved";
+//  }
+//
+//}

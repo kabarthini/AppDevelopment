5f5a00e50b9c2a6d37c83d79143b78c6480386fe
kabarthini <110531712+kabarthini@users.noreply.github.com>
2023-10-11 16:07:21 +0530

    Add files via upload

---
+++ Day19/weatherforecast/src/main/java/com/example/demo/workshop/service/AuthenticationService.java
@@ -0,0 +1,81 @@
+package com.example.demo.workshop.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.workshop.dto.request.AuthenticationRequest;
+import com.example.demo.workshop.dto.request.RegisterRequest;
+import com.example.demo.workshop.dto.response.AuthenticationResponse;
+import com.example.demo.workshop.entity.UserRegister;
+import com.example.demo.workshop.entity.enumerate.Role;
+import com.example.demo.workshop.repository.UserRegisterRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+	
+	private final UserRegisterRepository repository ; 
+	 private final PasswordEncoder passwordEncoder;
+	 private final JwtService jwtservice;
+	 private final AuthenticationManager authenticationManager;
+	public AuthenticationResponse register(RegisterRequest request) {
+		// TODO Auto-generated method stub
+	
+			
+	    var user = UserRegister.builder()
+	            .username(request.getUsername())
+	            .emailid(request.getEmailid())
+	            .mobileno(request.getMobileno())
+	            .password(passwordEncoder.encode(request.getPassword()))
+	            .role(Role.USER)
+	            .build();
+	    repository.save(user); 
+	    var jwtToken = jwtservice.generateToken(user);
+	    
+		return AuthenticationResponse.builder()
+				.token(jwtToken)
+				.build();
+	}
+
+	public AuthenticationResponse authenticate(AuthenticationRequest request) {
+		authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(
+				request.getUsername(),
+				request.getPassword()
+				)
+				);
+				var user = repository.findByUsername(request.getUsername())
+						.orElseThrow();
+				 var jwtToken = jwtservice.generateToken(user);
+				    
+					return AuthenticationResponse.builder()
+							.token(jwtToken)
+							.build();
+	}
+	public List<UserRegister> getRegister() 
+	{
+		return repository.findAll();
+	}
+
+	public void updateRegister(UserRegister registerEntity) 
+	{
+		repository.save(registerEntity);	
+	}
+
+	public void deleteRegister(long userRegId) 
+	{
+		
+		repository.deleteById(userRegId);
+	}
+	
+	
+	
+
+}
---
+++ Day19/weatherforecast/src/main/java/com/example/demo/workshop/service/LoginService.java
@@ -0,0 +1,25 @@
+package com.example.demo.workshop.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.demo.workshop.entity.Login;
+import com.example.demo.workshop.repository.LoginRepository;
+
+
+public class LoginService 
+{
+	@Autowired
+	private LoginRepository userrepo;
+	public List<Login> getUser() 
+	   {
+		  return userrepo.findAll();
+	   }
+	public void saveUser(Login le)
+	   {
+		  userrepo.save(le);
+	   }
+	
+
+}
